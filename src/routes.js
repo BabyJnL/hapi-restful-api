978c7777d24b596fa65cf412c13e5a9a78c49b28
BabyJnL <ariyapanna@outlook.com>
2024-04-20 12:15:11 +0700

    chore: added eslint and updated project structure

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: (req, h) => {
+
+        }
+    }
+]
+
+module.exports = routes;
